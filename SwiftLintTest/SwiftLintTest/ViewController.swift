a8f61bd08d3ccc003c9781d7ad2e126605b24ad0
Arslan Basit <arslan.basit@brainxtech.com>
2023-01-26 17:42:30 +0500

    test code

---
+++ SwiftLintTest/SwiftLintTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLintTest
+//
+//  Created by  on 26/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var swift_lint_test: Int = 0
+
+    var value: Int? = 10
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var newValue = value! + 2
+
+    }
+
+
+}
+
